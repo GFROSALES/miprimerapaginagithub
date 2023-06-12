f5256a8f33694578f4e589fa9399ff8b8e8a48dd
GFROSALES <gr777256@gmail.com>
2023-06-12 20:35:47 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Encuentra al elemento HTML (el Botón)mediante su ID
+const miBoton = document.getElementById(miBoton);
+// Define la funcion que se ejecutará cuando se haga click en el botón
+function handleClick(); {
+    alert( 'Hola! Has hecho click en el botón.' );
+    addEventListener  
+}
+ 
+
+
+    
